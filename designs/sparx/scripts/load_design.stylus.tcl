77db2357f0970c4e5e91dac5b5b793107c666371
snpagliarini <snpagliarini@gmail.com>
2024-05-17 13:35:18 -0400

    adding sparx design

---
+++ designs/sparx/scripts/load_design.stylus.tcl
@@ -0,0 +1,43 @@
+# this is provided by the contest organizer. not sure which version of innovus this works with. 
+# this script assumes innovus is launched with the -common_ui flag << this is important
+
+####
+# settings
+####
+set_multi_cpu_usage -local_cpu 8
+set_db design_process_node 45
+
+set mmmc_path ../originals/mmmc_ui.tcl
+set lef_path ../../../libraries/NangateOpenCellLibrary_metal1--metal6.lef
+set def_path ../originals/design_original.def
+set netlist_path ../originals/design_original.v
+
+####
+# init
+####
+read_mmmc $mmmc_path
+#read_physical -lefs $lef_path
+read_physical -lef $lef_path
+read_netlist $netlist_path
+read_def $def_path -preserve_shape
+
+init_design
+
+####
+# clock propagation
+####
+set_interactive_constraint_modes [all_constraint_modes -active]
+reset_propagated_clock [all_clocks]
+update_io_latency -adjust_source_latency -verbose
+set_propagated_clock [all_clocks]
+
+####
+# timing
+####
+set_db timing_analysis_type ocv
+set_db timing_analysis_cppr both
+time_design -post_route
+
+set DESIGN sparx
+
+## NOTE no exit here, as this is supposed to be sourced along with other scripts
