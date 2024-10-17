22e9e511df4408f5e15dbedebd67eb8ac3057d4c
snpagliarini <snpagliarini@gmail.com>
2024-10-17 09:55:53 -0400

    scripts for loading already implemented DBs

---
+++ designs/aes_2/scripts/load_final.stylus.tcl
@@ -0,0 +1,15 @@
+#### this script loads the finalized db, after security closure was applied
+set_multi_cpu_usage -local_cpu 8
+set_db design_process_node 45
+
+set DESIGN AES
+
+read_db ../db/aes.tar.gz
+
+set_db timing_analysis_type ocv
+set_db timing_analysis_cppr both
+time_design -post_route
+
+source ../scripts/eval.stylus.tcl
+
+
