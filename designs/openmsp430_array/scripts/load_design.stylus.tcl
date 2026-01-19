d6dea4eb62f5845d8ed1cb7697a5bbee91e5c71d
snpagliarini <snpagliarini@gmail.com>
2026-01-19 11:23:23 -0500

    scripts for MSP array implementation

---
+++ designs/openmsp430_array/scripts/load_design.stylus.tcl
@@ -0,0 +1,47 @@
+# this is provided by the contest organizer. not sure which version of innovus this works with. 
+# this script assumes innovus is launched with the -common_ui flag << this is important
+
+####
+# settings
+####
+set_multi_cpu_usage -local_cpu 8
+set_db design_process_node 45
+set_db init_design_uniquify true
+
+set mmmc_path ../originals/mmmc_ui.tcl
+set lef_path ../../../libraries/NangateOpenCellLibrary_metal1--metal6.lef
+set netlist_path ../originals/design_original.v
+
+####
+# init
+####
+read_mmmc $mmmc_path
+#read_physical -lefs $lef_path
+read_physical -lef $lef_path
+read_netlist $netlist_path
+
+set_db init_power_nets VDD
+set_db init_ground_nets VSS
+
+init_design
+
+####
+# clock propagation
+####
+set_interactive_constraint_modes [all_constraint_modes -active]
+reset_propagated_clock [all_clocks]
+update_io_latency -adjust_source_latency -verbose
+set_propagated_clock [all_clocks]
+
+####
+# timing
+####
+set_db timing_analysis_type ocv
+set_db timing_analysis_cppr both
+#time_design -post_route
+# this is important to make the power environment match the design settings
+set_default_switching_activity -sequential_activity -1
+
+set DESIGN openmsp430_array
+
+## NOTE no exit here, as this is supposed to be sourced along with other scripts
---
+++ designs/openmsp430_array/scripts/load_final.stylus.tcl
@@ -0,0 +1,18 @@
+#### this script loads the finalized db, after security closure was applied
+set_multi_cpu_usage -local_cpu 8
+set_db design_process_node 45
+
+set DESIGN openmsp430_2
+
+read_db ../db/openmsp2.tar.gz
+
+set_db timing_analysis_type ocv
+set_db timing_analysis_cppr both
+time_design -post_route
+
+# this is important to make the power environment match the design settings
+set_default_switching_activity -sequential_activity -1
+
+source ../scripts/eval.stylus.tcl
+
+
